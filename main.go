eae97276d72e072d57a9a497bf3af71342134c1c
Ivan de la Rubia <ivandelarubia@gmail.com>
2017-02-12 04:09:39 +0100

    listen on port envvar

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	_ "mpaths-web/routers"
+	"os"
+	"strconv"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.BConfig.Listen.HTTPPort, _ = strconv.Atoi(os.Getenv("PORT"))
 	beego.Run()
 }
-
